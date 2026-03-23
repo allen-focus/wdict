77223c8f2a110f7cb5dbfa9d386d592ae00744f8
Allen Bob <boballen1996815@gmail.com>
2026-03-23 16:51:22 +0800

    misc: adjust variable name of utf8 function

--- unicode.c
+++ unicode.c
@@ -7,7 +7,7 @@
 //   3. Does not support multi‑codepoint sequences.
 byte* utf8_decode(byte* str, u32* codepoint)
 {
-    u8 ns[] = {
+    u8 length_table[] = {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 0xxxx
         0, 0, 0, 0, 0, 0, 0, 0,                          // 10xxx
         2, 2, 2, 2,                                      // 110xx
@@ -18,16 +18,16 @@ byte* utf8_decode(byte* str, u32* codepoint)
     byte mask[] = { 0, 0x7F, 0x1F, 0x0F, 0x07 };
     u8 shift[] = { 0, 18, 12, 6, 0 };
 
-    u8 n = ns[str[0] >> 3];
-    *codepoint = (str[0] & mask[n]) << 18;
-    Assert(n > 0);
-    switch (n) {
+    u8 length = length_table[str[0] >> 3];
+    Assert(length > 0);
+    *codepoint = (str[0] & mask[length]) << 18;
+    switch (length) {
         case 4: *codepoint |= str[3] & 0x3F;
         case 3: *codepoint |= (str[2] & 0x3F) << 6;
         case 2: *codepoint |= (str[1] & 0x3F) << 12;
-        default: *codepoint >>= shift[n];
+        default: *codepoint >>= shift[length];
     }
-    return str + n;
+    return str + length;
 }
 
 // NOTE:
@@ -35,24 +35,24 @@ byte* utf8_decode(byte* str, u32* codepoint)
 //   2. Does not support multi‑codepoint sequences.
 isize utf8_encode(byte* str, u32 codepoint)
 {
-    isize n = 0;
+    isize length = 0;
     if (codepoint < (1 << 8))
     {
         str[0] = codepoint;
-        n = 1;
+        length = 1;
     }
     else if (codepoint < (1 << 11))
     {
         str[0] = 0xC0 | (codepoint >> 6);
         str[1] = 0x80 | (codepoint & 0x3F);
-        n = 2;
+        length = 2;
     }
     else if (codepoint < (1 << 16))
     {
         str[0] = 0xE0 | (codepoint >> 12);
         str[1] = 0x80 | ((codepoint >> 6) & 0x3F);
         str[2] = 0x80 | (codepoint & 0x3F);
-        n = 3;
+        length = 3;
     }
     else if (codepoint < (1 << 21))
     {
@@ -60,11 +60,11 @@ isize utf8_encode(byte* str, u32 codepoint)
         str[1] = 0x80 | ((codepoint >> 12) & 0x3F);
         str[2] = 0x80 | ((codepoint >> 6) & 0x3F);
         str[3] = 0x80 | (codepoint & 0x3F);
-        n = 4;
+        length = 4;
     }
     else
     {
         Assert(0);
     }
-    return n;
+    return length;
 }
